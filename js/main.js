754e30f4a4ff56b94ff60a48b4e86870bd7f0997
shermatovelbek <shermatovelbek772@gmail.com>
2023-12-06 21:11:48 +0500

    6-misol ohirigacha ishlab tugatildi

--- js/main.js
+++ js/main.js
@@ -242,8 +242,6 @@
 
 //4-oy 6-dars vazifasi
 
-
-
 //1-misol
 
 // let a = 10;
@@ -269,37 +267,170 @@
 //   if(i % 3 == 0){
 //     sum = sum * i;
 //   }
-// } 
+// }
 // console.log(sum)
 
-
 //3-misol
-let a = 15;
-let b = 10;
-let i = 0;
-let count = 0;
-while(i <= a){
-  i++;
-  if(i % 3 == 0 ){
-    count++;
-  }
+// let a = 5;
+// let b = 10;
+// let i = 0;
+// let count = 0;
+// while(i <= a){
+//   i++;
+//   if(i % 3 == 0 ){
+//     count++;
+//   }
+// }
+// while(i <= a){
+//   i++;
+//   if(i % 7 == 0 ){
+//     count++;
+//   }
+// }
+// while(i <= b){
+//   i++;
+//   if(i % 3 == 0 ){
+//     count++;
+//   }
+// }
+// while(i <= b){
+//   i++;
+//   if(i % 7 == 0 ){
+//     count++;
+//   }
+// }
+// console.log(count)
+
+//4-misol
+// let a = 7;
+// let sum;
+
+// if(a == 2){
+//   sum = "Bu son tub son emas !!!";
+// } else{
+//   sum = "Bu son tub son  !!!";
+// }
+// console.log(sum);
+
+//5-misol
+
+// let a = 153;
+
+// let b = (a % 10);
+// let o = (a / 10) - a % 10 / 10 - 10;
+// let y = ( a / 10) - a % 10 / 10 - 14;
+// sum = b + o + y;
+// console.log(sum);
+
+//6-misol
+
+let a = 153;
+let y = a / 10 - (a % 10) / 10 - 14;
+let o = a / 10 - (a % 10) / 10 - 10;
+let b = a % 10;
+
+let birlik = "";
+let onlik = "";
+let yuzlik = "";
+switch (y) {
+  case 100:
+    yuzlik = "biryuzi";
+    break;
+  case 200:
+    yuzlik = "ikkiyuzi";
+    break;
+  case 300:
+    yuzlik = "uchyuzi";
+    break;
+  case 400:
+    yuzlik = "to'rtyuzi";
+    break;
+  case 500:
+    yuzlik = "beshyuzi";
+    break;
+  case 600:
+    yuzlik = "oltyuzi";
+    break;
+  case 700:
+    yuzlik = "yetyuzi";
+    break;
+  case 800:
+    yuzlik = "sakkizyuzi";
+    break;
+  case 900:
+    yuzlik = "to'qqizyuzi";
+    break;
+
+  default:
+    sum = "Ushbu operatsiyani amalga oshirib bo'lmaydi !!!"
+    break;
+};
+switch (o) {
+  case 10:
+    onlik += "o'n";
+    break;
+  case 20:
+    onlik += "yigirma";
+    break;
+  case 30:
+    onlik += "o'ttiz";
+    break;
+  case 40:
+    onlik += "qirq";
+    break;
+  case 50:
+    onlik += "ellik";
+    break;
+  case 60:
+    onlik += "oltmish";
+    break;
+  case 70:
+    onlik += "yetmish";
+    break;
+  case 80:
+    onlik += "sakson";
+    break;
+  case 90:
+    onlik += "to'qson";
+    break;
+
+  default:
+    sum = "Ushbu operatsiyani amalga oshirib bo'lmaydi !!!"
+    break;
 }
-while(i <= a){
-  i++;
-  if(i % 7 == 0 ){
-    count++;
-  }
+switch (b) {
+  case 1:
+    birlik += "bir";
+    break;
+  case 2:
+    birlik += "ikki";
+    break;
+  case 3:
+    birlik += "uch";
+    break;
+  case 4:
+    birlik += "to'rt";
+    break;
+  case 1:
+    birlik += "besh";
+    break;
+  case 1:
+    birlik += "olti";
+    break;
+  case 1:
+    birlik += "yetti";
+    break;
+  case 1:
+    birlik += "sakkiz";
+    break;
+  case 1:
+    birlik += "qo'qqiz";
+    break;
+
+  default:
+    sum =  "Ushbu operatsiyani amalga oshirib bo'lmaydi !!!"
+    break;
 }
-while(i <= b){
-  i++;
-  if(i % 3 == 0 ){
-    count++;
-  }
-}
-while(i <= b){
-  i++;
-  if(i % 7 == 0 ){
-    count++;
-  }
-}
-console.log(count)
+
+
+console.log(yuzlik , onlik , birlik)
